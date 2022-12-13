a24dd5c89a1013d37a748d71226ff6094acaeb6a
Jabullae <yoyoui54@gmail.com>
2022-12-13 16:27:37 +0900

    file update

--- FlaskIntro/static/app.py
+++ FlaskIntro/static/app.py
@@ -1,13 +1,31 @@
+# https://markupsafe.palletsprojects.com/en/2.0.x/
+from markupsafe import escape
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+def index():
+    return 'Hello World!'
 
-@app.route('/busan')
-def amu():
-    return '역시 부산아인교!'
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/user/<username>/')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>/')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+
+@app.route('/path/<path:subpath>/')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)   # http://localhost:5000/path/lookup/<br> 입력으로 escape 함수 효과 확인!
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
