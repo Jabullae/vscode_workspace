f7fbfcd67947820554f281a7ba744da392c84d2b
Jabullae <yoyoui54@gmail.com>
2022-12-14 11:30:21 +0900

    221214 flask

--- FlaskIntro/app.py
+++ FlaskIntro/app.py
@@ -1,33 +1,16 @@
-
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return '<html><body><h1>Hello World</h1></body></html>'
 
     
-@app.route('/login/')
-def login():
-    return render_template('login.html')
+@app.route('/hello/<user>')
+def hello_name(user):
+    return render_template('hello.html', name = user)
 
-
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
-
-
-@app.route('/auth', methods=['POST', 'GET'])
-def auth():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))        
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user+' by Get'))
-
-
+    
 if __name__ == '__main__':
     app.run(debug=True)
-
